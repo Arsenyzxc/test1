77e3a6b82c4d03e95aa79f91a0984ba068d894de
Artyom <worldbeater-dev@yandex.ru>
2025-09-18 10:15:40 +0300

    task1: repl

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print('привет')
+import argparse
+
+def repl(vfs_name: str):
+    """
+    Минимальный REPL-цикл: приглашение, парсер, команды-заглушки.
+    """
+    while True:
+        # приглашение вида [VFS]$
+        line = input(f"[{vfs_name}]$ ").strip()
+
+        if not line:
+            continue
+
+        parts = line.split()
+        cmd = parts[0]
+        args = parts[1:]
+
+        # команды
+        if cmd == "exit":
+            print("Выход из эмулятора")
+            break
+        elif cmd in ("ls", "cd"):
+            print(f"Вызвана команда: {cmd}, аргументы: {args}")
+        else:
+            print(f"Ошибка: неизвестная команда '{cmd}'")
+
+
+def main():
+    repl('VFS')
+
+
+if __name__ == "__main__":
+    main()
